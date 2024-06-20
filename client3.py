03b80b3ecc512d9122b629d3dfa2ccf2cd1c1e9f
Najia <naziausha@gmail.com>
2024-06-20 14:36:14 -0400

    task-1

--- client3.py
+++ client3.py
@@ -31,18 +31,20 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
+
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None  # Handle division by zero gracefully, returning None here
+    return price_a / price_b
+
 
 
 # Main
@@ -51,9 +53,21 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Ensure you have at least two stocks in prices dictionary for ratio calculation
+        if len(prices) >= 2:
+            # Select any two stocks dynamically for ratio calculation
+            stock_keys = list(prices.keys())
+            stock_a = stock_keys[0]
+            stock_b = stock_keys[1]
+
+            ratio = getRatio(prices[stock_a], prices[stock_b])
+            if ratio is not None:
+                print("Ratio of %s to %s: %s" % (stock_a, stock_b, ratio))
+        else:
+            print("Not enough stocks in prices dictionary to calculate ratio.")
